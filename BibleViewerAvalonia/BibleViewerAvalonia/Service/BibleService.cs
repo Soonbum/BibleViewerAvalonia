e93e02c786fa4fe30e6b833cd91418533474935e
Soonbum Jeong <peacemaker84@gmail.com>
2025-09-29 14:45:35 +0900

    BibleService.cs, Text 디렉토리
    BibleService.cs
    - 성경역본 리스트 업데이트
    
    Text 디렉토리
    - 성경역본 데이터 추가

--- BibleViewerAvalonia/BibleViewerAvalonia/Service/BibleService.cs
+++ BibleViewerAvalonia/BibleViewerAvalonia/Service/BibleService.cs
@@ -14,8 +14,14 @@ public partial class BibleService
         // 나중에 이 부분은 파일이나 데이터베이스에서 데이터를 읽어오도록 수정될 수 있습니다.
         return
         [
-            "킹제임스흠정역",
-            "개역한글판",
+            "킹제임스흠정역", // korhkjv
+            "개역한글", // korhrv
+            "개역개정", // kornkrv
+            "바른성경", // korktv
+            "쉬운성경", // koreasy
+            "한글킹제임스", // korkkjv
+            "현대어성경", // kortkv
+            "King James Version", // engkjv
         ];
     }
 
